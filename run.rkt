6e497c8d51b059f07863cb2d0b4ea15d2417c95f
Phitchaya Mangpo Phothilimthana <pphothil@qti.qualcomm.com>
2014-07-01 21:28:23 -0700

    more test programs in run.rkt

--- run.rkt
+++ run.rkt
@@ -2,13 +2,38 @@
 
 (require "main.rkt")
 
-(define code 
-"
-	movsi LR4, -1
-	and LR3, LR1, LR0
-	xor LR1, LR0, LR4
-	and LR2, LR2, LR1
-	or LR0, LR2, LR3
-")
+;; md5
+;; (define code 
+;; "
+;; 	movsi LR4, -1
+;; 	and LR3, LR1, LR0
+;; 	xor LR1, LR0, LR4
+;; 	and LR2, LR2, LR1
+;; 	or LR0, LR2, LR3
+;; ")
 
-(optimize code 5 1 (list 0) (list) #f #:dir "test")
+;; bset
+;; (define code
+;; "	movsi LR2, 1
+;; 	sll LR3, LR2, LR1
+;; 	or LR0, LR3, LR0
+;; ")
+
+;; p13_sign
+;; (define code
+;; "	movsi LR1, 0
+;; 	sub LR2, LR1, LR0
+;; 	or LR3, LR0, LR2
+;; 	srai LR0, LR3, 31")
+
+;; p16_max
+(define code
+"xor LR2, LR0, LR1
+sgeu LR3, LR0, LR1
+movsi LR4, 0
+andi LR3, LR3, 1
+sub LR3, LR4, LR3
+and LR0, LR2, LR3
+xor LR0, LR0, LR1")
+
+(optimize code 5 1 (list 0) (list) #t #:dir "p13_sign1" #:cores 12)
