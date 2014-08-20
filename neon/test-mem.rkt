73705d1e20cb00ac07ae5ecc848d1ddbae1da3e6
Phitchaya Mangpo Phothilimthana <pphothil@qti.qualcomm.com>
2014-08-20 13:49:35 -0700

    with facket simulator

--- neon/test-mem.rkt
+++ neon/test-mem.rkt
@@ -1,13 +1,15 @@
 #lang s-exp rosette
 
 (require "neon-machine.rkt" "neon-printer.rkt"
-         "neon-parser.rkt" "neon-simulator-rosette.rkt")
+         "neon-parser.rkt" 
+         "neon-simulator-rosette.rkt" "neon-simulator-racket.rkt")
 
 
 (define parser (new neon-parser%))
 (define machine (new neon-machine%))
 (define printer (new neon-printer% [machine machine]))
 (define simulator (new neon-simulator-rosette% [machine machine]))
+(define simulator-racket (new neon-simulator-racket% [machine machine]))
 
 (define code
 (send parser ast-from-string "
@@ -24,10 +26,19 @@ VMLAL.S16 q0, d3, d2[0]
   (define-symbolic* input number?)
   input)
 
-(define t (current-seconds))
-(for ([i 2000])
+(define (input) 0)
+
+(define (loop)
   (pretty-display `(mem ,(quotient (current-memory-use) 1000)))
+
+  ;; Rosette
   (let ([state (default-state machine sym-input)])
     (send simulator interpret encoded-code state))
-)
-(pretty-display `(time ,(- (current-seconds) t)))
+
+  ;; Racket
+  ;; (let ([state (default-state machine input)])
+  ;;   (send simulator-racket interpret encoded-code state))
+  (clear-asserts)
+
+  (loop))
+(loop)
