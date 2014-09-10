71cffeaec1c395770a4d5ba97555caca074a37a9
Phitchaya Mangpo Phothilimthana <mangpo@eecs.berkeley.edu>
2014-09-09 22:57:45 -0700

    test-parallel: neon benchmark

--- neon/test-parallel.rkt
+++ neon/test-parallel.rkt
@@ -8,11 +8,11 @@
 (send parser ast-from-string "
 vorr q3, q0, q0
 vld1 {d4,d5}, [r0]
-vmov q1, q4
-vswp d2, d3
+vorr d2, d9, d9
+vorr d3, d8, d8
 vbsl q3, q1, q2
 vst1.32	{d6,d7}, [r0]
 "))
 
-(optimize code (list (list) (list)) 
-          #t #:time-limit 36000 #:cores 12)
+(optimize code (list (list 0 1) (list)) 
+          #t #:time-limit 72000 #:cores 8 #:dir "output4")
