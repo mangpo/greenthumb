bdc0d705e8da1ac562439d3fad1a86bd59bf4498
mangpo <mangpo@eecs.berkeley.edu>
2014-09-17 22:13:42 -0700

    GA: has zero byte benchmark

--- GA/run.rkt
+++ GA/run.rkt
@@ -3,15 +3,29 @@
 (require "main.rkt" "GA-parser.rkt")
 
 (define parser (new GA-parser%))
+;; fff
 ;; (optimize (send parser ast-from-string 
 ;;                 "push over - push and pop pop and over 65535 or and or")
 ;;           '((data . 2) memory)
 ;;           #t #t 0 #:cores 8 #:time-limit 3600 #:size 6
 ;;           #:assume '((<= . 65535) (<= . 65535) (<= . 65535)))
 
-(optimize (send parser ast-from-string "2 b! @b 3 b! !b 1 b! @b 2 b! !b")
-          '((data . 2) memory)
-          #t #t 0 #:cores 8 #:time-limit 3600 #:size 8)
+;; mem
+;; (optimize (send parser ast-from-string "2 b! @b 3 b! !b 1 b! @b 2 b! !b")
+;;           '((data . 2) memory)
+;;           #t #t 0 #:cores 8 #:time-limit 3600 #:size 8)
+
+;; has zero byte?
+;; (optimize (send parser ast-from-string 
+;;                 "dup 32639 and 32639 + over over - and + 32639 over - and + -")
+;;           '((data . 1))
+;;           #t #t 0 #:cores 8 #:time-limit 3600 #:size 6
+;;           #:assume '((<= . 65535)))
+
+(optimize (send parser ast-from-string 
+                "over - and + - push drop pop")
+          '((data . 1))
+          #t #t 0 #:cores 8 #:time-limit 3600 #:size 5)
 
 ;; (optimize (send parser ast-from-string "dup drop 2 a! !") 
 ;;           '((data . 2) memory)
