1b9757646f1fc5786dcf26453412d79cbf71e01a
mangpo <mangpo@eecs.berkeley.edu>
2014-10-10 10:19:17 -0700

    ARM: fix printer

--- arm/test-simulator.rkt
+++ arm/test-simulator.rkt
@@ -22,13 +22,23 @@
 ;; Section 1: Concrete program
 (define code
 (send parser ast-from-string "
-        cmp     r0, r1
-        movne   r2, r3
-        cmp     r0, r3
-        eoreq   r0, r3, r1
-        movne   r0, #0
-        eor     r0, r2, r0
-
+	movw	r1, #21845
+	bfi	r1, r1, #16, #16
+	and	r1, r1, r0, asr #1
+	movw	r2, #13107
+	rsb	r1, r1, r0
+	movt	r2, 13107
+	movw	r3, #13107
+	bfi	r3, r3, #16, #16
+	and	r2, r1, r2
+	and	r1, r3, r1, asr #2
+	add	r2, r2, r1
+	movw	r3, #3855
+	movt	r3, 3855
+	add	r2, r2, r2, asr #4
+	and	r3, r2, r3
+	add	r0, r3, r3, lsr #8
+	add	r0, r0, r0, lsr #16
 "))
 (send printer print-struct code)
 (define encoded-code (send printer encode code))
