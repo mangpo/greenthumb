a73343c7087841646f8322706a2b4d943ba320c2
mangpo <mangpo@eecs.berkeley.edu>
2014-10-15 13:34:08 -0700

    ARM: run.sh

--- arm/run.sh
+++ arm/run.sh
@@ -1,12 +1,12 @@
-type=basic
+type=livein_half
 for name in p22_parity #p23_count p18_is_power p21_cycle p22_parity 
 do
     for mode in s #o
     do
-	for t in 1 2 3
+	for t in 1 2
 	do
 	    echo $name
-	    racket optimize.rkt --live-reg 0 --stoch -$mode -c 8 -t 3600 -d results/$name-$type-$mode-$t programs/$name.s > results/$name-$type-$mode-$t.log
+	    racket optimize.rkt --live-out 0 --live-in 0 --stoch -$mode -c 8 -t 3600 -d results/$name-$type-$mode-$t programs/$name.s > results/$name-$type-$mode-$t.log
 	done
     done
 done
