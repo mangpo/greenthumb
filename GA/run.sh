83b76e5a4e33b07241f8714edbb179a401bbab5a
Phitchaya Mangpo Phothilimthana <mangpo@eecs.berkeley.edu>
2014-10-19 10:41:38 -0700

    GA: run.sh script

--- GA/run.sh
+++ GA/run.sh
@@ -1,11 +1,11 @@
-type=hybrid
+type=hybrid+1p+half
 mode=s
-for name in complexA complexB complexC fir rrotate shaf
+for name in complexC
 do
     for t in 1 2
     do
 	echo "$name $t"
-	racket optimize.rkt --hybrid -$mode -c 8 -t 100 -d results/$name-$type-$mode-$t programs/$name.s > results/$name-$type-$mode-$t.log
+	racket optimize.rkt --hybrid -$mode -c 24 -t 150 -d results/$name-$type-$mode-$t programs/$name.s > results/$name-$type-$mode-$t.log
     done
 done
 
