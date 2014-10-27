54afc8f8f4c281dd8544172835974f45bff6da0b
mangpo <mangpo@eecs.berkeley.edu>
2014-10-27 15:18:58 -0700

    ARM: modify run_wb.sh

--- arm/run_wb1.sh
+++ arm/run_wb1.sh
@@ -2,14 +2,14 @@ extra=livein+half
 
 for name in wb_txrate5a                                           
 do
-    for type in stoch hybrid
+    for type in hybrid
     do
     for mode in s #o                                                                
     do
         for t in 1 2
         do
         echo "$name mode=$mode"
-        racket optimize.rkt --live-out 1 --live-in 1 --dead-mem --$type -$mode -c 8 -t 3600 -d results/$name-$type+$extra-$mode-$t programs/$name.s > results/$name-$type+$extra-$mode-$t.log
+        racket optimize.rkt --live-out 1,3 --live-in 1 --dead-mem --$type -$mode -c 8 -t 3600 -d results/$name-$type+$extra-$mode-$t programs/$name.s > results/$name-$type+$extra-$mode-$t.log
         done
     done
     done
