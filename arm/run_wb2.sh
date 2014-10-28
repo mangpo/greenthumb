623952d05f60dd5898ece142abf4149162252c9d
mangpo <mangpo@eecs.berkeley.edu>
2014-10-27 22:57:35 -0700

    ARM: fix run_wb2

--- arm/run_wb2.sh
+++ arm/run_wb2.sh
@@ -9,10 +9,11 @@ do
         for t in 1 2
         do
         echo "$name mode=$mode"
-        racket optimize.rkt --live-out 2,3,6,10,11 --live-in 1 --dead-mem --$type -$mode -c 8 -t 3600 -d results/$name-$type+$extra-$mode-$t programs/$name.s > results/$name-$type+$extra-$mode-$t.log
+        racket optimize.rkt --live-out 1 --live-in 2,3,6,10,11 --dead-mem --$type -$mode -c 8 -t 3600 -d results/$name-$type+$extra-$mode-$t programs/$name.s > results/$name-$type+$extra-$mode-$t.log
         done
     done
     done
 done
 
-sh run_wb3.sh
+#sh run_wb3.sh
+sh dummy2.sh
