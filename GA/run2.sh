0ed71d12b59fb0387aa21cf67188c14a46df3aee
mangpo <mangpo@eecs.berkeley.edu>
2014-11-02 14:48:42 -0800

    GA: run2.sh for Amazon

--- GA/run2.sh
+++ GA/run2.sh
@@ -1,12 +1,6 @@
-type=hybrid+1p+half
-mode=s
-for name in complexB fir
+for name in fff ggg hhh iii
 do
-    for t in 1 2
-    do
-	echo "$name $t"
-	racket optimize.rkt --hybrid -$mode -c 24 -t 500 -d results/$name-$type-$mode-$t programs/$name.s > results/$name-$type-$mode-$t.log
-    done
+    bash run.sh $name 3600
 done
 
-sh run3.sh
+sh dummy.sh
