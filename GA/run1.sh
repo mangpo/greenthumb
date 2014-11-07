ccc671a21046a69de1c52b8da2f6457c9d52bfc3
mangpo <mangpo@eecs.berkeley.edu>
2014-11-06 22:32:57 -0800

    GA: run extra

--- GA/run1.sh
+++ GA/run1.sh
@@ -2,5 +2,6 @@ for name in shag complexC iii_ex
 do
     bash run.sh $name 1200
 done
+# 7 hours each
 
 sh dummy.sh
--- GA/run2.sh
+++ GA/run2.sh
@@ -1,6 +1,7 @@
-bash run.sh fff 900
-bash run.sh ggg 1800
-bash run.sh hhh 900
-bash run.sh iii 1900
+for name in fir shaf complexA complexB rrotate interp fff ggg hhh iii 
+do
+    bash run_extra.sh $name 1200
+done
+# 3 hours each = 30 hours
 
-bassh dummy.sh
+bash dummy.sh
