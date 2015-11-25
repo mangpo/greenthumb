06674094ddcfd6c7be91df374f3c719ab468658b
mangpo <mangpo@eecs.berkeley.edu>
2015-11-25 13:57:15 -0800

    forget to add simulator-racket/rosette

---
+++ simulator-racket.rkt
@@ -0,0 +1,8 @@
+#lang racket
+
+(require "simulator.rkt")
+(provide simulator-racket%)
+
+(define simulator-racket%
+  (class simulator%
+    (super-new)))
---
+++ simulator-rosette.rkt
@@ -0,0 +1,8 @@
+#lang s-exp rosette
+
+(require "simulator.rkt")
+(provide simulator-rosette%)
+
+(define simulator-rosette%
+  (class simulator%
+    (super-new)))
